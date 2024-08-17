1d157f57138785c810ac4e8f2ead2ccb5ade547c
varaprasad083 <varaprasadsaragada083@gmail.com>
2024-08-17 09:36:16 +0530

    error fixed

--- tests/orangedemo.spec.js
+++ tests/orangedemo.spec.js
@@ -15,10 +15,7 @@ test.beforeEach("OrangeHRM", async({page})=>{
 
 test("OrangeHRM assignLeave", async({page})=>{
     await page.locator("button[title='Assign Leave']").click()
-    await page.getByPlaceholder('Type for hints...').fill('vara');
     await page.waitForTimeout(3000)
-    await page.getByRole('option', { name: 'vara Prasad Saragada' }).click();
-
     await page.locator('form i').first().click();
     await page.getByRole('option', { name: 'CAN - Personal' }).click();
     await page.locator('form i').nth(2).click();
@@ -31,21 +28,28 @@ test("OrangeHRM assignLeave", async({page})=>{
     await page.getByRole('option',{name:'Half Day - Afternoon'}).click()
     await page.locator(".oxd-textarea.oxd-textarea--active.oxd-textarea--resize-vertical").fill("leave is for my personal reasons")
     await page.getByRole('button',{name:'Assign'}).click()
-    await page.getByRole('button',{name:'Ok'}).click()
  
 })
 
-test("OrangeHRM Admin",async({page})=>{
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers")
-    
-    await page.locator("div[class='oxd-input-group oxd-input-field-bottom-space'] div input[class='oxd-input oxd-input--active']").fill('Admin');
-    await page.locator('form i').first().click()
-    await page.getByRole('option',{name:'Admin'}).click()
-    await page.getByPlaceholder('Type for hints...').fill('an');
-    await page.getByText('aniket Ashok patil').click();
-    await page.locator('form i').nth(1).click()
-    await page.getByRole('option',{name:'Enabled'}).click()
-    await page.getByRole('button',{name:'Search'}).click()
+test("OrangeHRM Recuirment",async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates")
+
+    await page.locator('.oxd-select-text--after > .oxd-icon').first().click();
+    await page.getByText('Chief Executive Officer').click();
+    await page.locator('form i').nth(1).click();
+    await page.getByRole('option', { name: 'QA lead' }).click();
+    await page.locator('form i').nth(2).click();
+    await page.getByRole('option', { name: 'manda user' }).click();
+    await page.locator('form i').nth(3).click();
+    await page.getByRole('option', { name: 'Application Initiated' }).click();
+    await page.getByPlaceholder('Enter comma seperated words...').fill('nothing is better');
+    await page.locator('.oxd-date-input > .oxd-icon').first().click();
+    await page.getByText('19').click();
+    await page.locator('.oxd-date-input > .oxd-icon').nth(1).click();
+    await page.getByText('22').click();
+    await page.locator('div:nth-child(3) > .oxd-grid-4 > .oxd-grid-item > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click();
+    await page.getByRole('option', { name: 'Manual' }).click();
+    await page.getByRole('button', { name: 'Search' }).click();
 })
 
 test("OrangeHRM PIM", async({page})=>{
